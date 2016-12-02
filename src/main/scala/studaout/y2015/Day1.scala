82bd63ecfe12ffc413f67f99afd8f6a6ccad8bc0
studaout <studaout@gmail.com>
2016-12-02 11:46:33 -0500

    2015

---
+++ src/main/scala/studaout/y2015/Day1.scala
@@ -0,0 +1,24 @@
+package studaout.y2015
+
+import studaout._
+
+object Day1 {
+
+  val inputFile = "/2015/day1-input.txt"
+  val line: String = firstLine(inputFile)
+
+  def main(args: Array[String]) {
+    val res = line.foldLeft((0,0, false))( (i, c) => {
+      val floor = c match {
+        case '(' => i._1+1
+        case ')' => i._1-1
+      }
+      val step = if (i._3) i._2 else i._2 + 1
+      if ( i._3 || floor < 0) (floor, step, true) else (floor, step, false)
+    })
+
+    println("target floor: " + res._1)
+    println("basement at step: " + res._2)
+  }
+
+}
