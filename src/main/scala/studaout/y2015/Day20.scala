1e6be892c4b420c5edcaf1d41c348e0249feecca
studaout <studaout@gmail.com>
2016-12-03 11:04:12 -0500

    2015

---
+++ src/main/scala/studaout/y2015/Day20.scala
@@ -0,0 +1,37 @@
+package studaout.y2015
+
+/**
+  * Created by timout on 12/25/15.
+  */
+object Day20 {
+
+  def part1(): Unit = {
+    val num = 34000000L
+    val size = 1000000
+    var arr = new Array[Long](size+1)
+    for (i <- 1 to size){
+      for ( j <- i to size by i) {
+        arr(j) += i*10
+      }
+    }
+    println(arr.indexOf(arr.find( _ >= num).getOrElse(-1L)))
+  }
+
+  def part2(): Unit = {
+    val num = 34000000L
+    val size = 1000000
+    var arr = new Array[Long](size+1)
+    for (i <- 1 to size){
+      for ( j <- i to i*50 by i) {
+        if ( j <= size ) arr(j) += i*11
+      }
+    }
+    println(arr.indexOf(arr.find( _ >= num).getOrElse(-1L)))
+  }
+
+  def main(args: Array[String]) {
+    part1()
+    part2()
+  }
+
+}
