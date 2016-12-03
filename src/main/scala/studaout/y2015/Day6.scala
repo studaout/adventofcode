4ec67b33a16c1bccaf5b0e5e261c54e4071415b3
studaout <studaout@gmail.com>
2016-12-03 11:09:27 -0500

    2015

---
+++ src/main/scala/studaout/y2015/Day6.scala
@@ -0,0 +1,56 @@
+package studaout.y2015
+
+import studaout._
+
+object Day6 {
+
+  def part1(): Unit = {
+    val inputFile = "/2015/day6-input.txt"
+    val size = 1000
+    val mat = Array.ofDim[Int](size, size)
+    for ( line <- lines(inputFile) ) {
+      line match {
+        case r"turn off (\d+)${x1},(\d+)${y1} through (\d+)${x2},(\d+)${y2}" =>
+          for ( x<-x1.toInt to x2.toInt; y<- y1.toInt to y2.toInt ) mat(x)(y) = 0
+        case r"turn on (\d+)${x1},(\d+)${y1} through (\d+)${x2},(\d+)${y2}" =>
+          for ( x<-x1.toInt to x2.toInt; y<- y1.toInt to y2.toInt ) mat(x)(y) = 1
+        case r"toggle (\d+)${x1},(\d+)${y1} through (\d+)${x2},(\d+)${y2}" =>
+          for ( x<-x1.toInt to x2.toInt; y<- y1.toInt to y2.toInt ) {
+            val v = mat(x)(y)
+            mat(x)(y) = if ( v == 0 ) 1 else 0
+          }
+        case _ =>
+      }
+    }
+    var count = 0
+    for ( x<-0 until size; y<-0 until size ) count += mat(x)(y)
+    println(count)
+  }
+
+  def part2(): Unit = {
+    val inputFile = "/2015/day6-input.txt"
+    val size = 1000
+    val mat = Array.ofDim[Int](size, size)
+    for ( line <- lines(inputFile) ) {
+      line match {
+        case r"turn off (\d+)${x1},(\d+)${y1} through (\d+)${x2},(\d+)${y2}" =>
+          for ( x<-x1.toInt to x2.toInt; y<- y1.toInt to y2.toInt ) {
+            if ( mat(x)(y) > 0 ) mat(x)(y) -=1
+          }
+        case r"turn on (\d+)${x1},(\d+)${y1} through (\d+)${x2},(\d+)${y2}" =>
+          for ( x<-x1.toInt to x2.toInt; y<- y1.toInt to y2.toInt ) mat(x)(y) += 1
+        case r"toggle (\d+)${x1},(\d+)${y1} through (\d+)${x2},(\d+)${y2}" =>
+          for ( x<-x1.toInt to x2.toInt; y<- y1.toInt to y2.toInt ) mat(x)(y) += 2
+        case _ =>
+      }
+    }
+    var count = 0
+    for ( x<-0 until size; y<-0 until size ) count += mat(x)(y)
+    println(count)
+  }
+
+  def main(args: Array[String]) {
+    part1()
+    part2()
+  }
+}
