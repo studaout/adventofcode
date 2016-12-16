fb7d578595cc2634e02459158bd53eac429e1777
studaout <studaout@gmail.com>
2016-12-16 10:18:48 -0500

    2015

--- src/main/scala/studaout/y2016/Day16.scala
+++ src/main/scala/studaout/y2016/Day16.scala
@@ -2,6 +2,9 @@ package studaout.y2016
 
 import studaout._
 
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
 object Day16 {
 
   val inputFile = "/2016/day16-input.txt"
@@ -23,6 +26,26 @@ object Day16 {
     a.substring(0, targetLength)
   }
 
+  /**
+    * Start with an appropriate initial state (your puzzle input). Then, so long as you don't have enough data yet to fill the disk,
+    * repeat the following steps:
+    *  Call the data you have at this point "a".
+    *  Make a copy of "a"; call this copy "b".
+    *  Reverse the order of the characters in "b".
+    *  In "b", replace all instances of 0 with 1 and all 1s with 0.
+    *  The resulting data is "a", then a single 0, then "b".
+    *
+    * Repeat these steps until you have enough data to fill the desired disk.
+    * once the data has been generated, you also need to create a checksum of that data. Calculate the checksum only for the data that
+    * fits on the disk, even if you generated more data than that in the previous step.
+    * The checksum for some given data is created by considering each non-overlapping pair of characters in the input data.
+    * If the two characters match (00 or 11), the next checksum character is a 1. If the characters do not match (01 or 10),
+    * the next checksum character is a 0. This should produce a new string which is exactly half as long as the original.
+    * If the length of the checksum is even, repeat the process until you end up with a checksum with an odd length.
+    *
+    * The first disk you have to fill has length 272. Using the initial state in your puzzle input, what is the correct checksum?
+    */
+
   def part1(): Unit = {
     var isOdd = false
     var checkSum = getStr(length1)
@@ -38,6 +61,11 @@ object Day16 {
     }
     println(checkSum)
   }
+
+  /**
+    * The second disk you have to fill has length 35651584. Again using the initial state in your puzzle input,
+    * what is the correct checksum for this disk?
+    */
 
   def part2(): Unit = {
     var isOdd = false
@@ -56,9 +84,38 @@ object Day16 {
     println(checkSum)
   }
 
+  implicit val ec = ExecutionContext.global
+
+
+  def calculateSum(s: Int, f: Int, str: String): String = {
+    var  i = s
+    val buf = new StringBuilder()
+    while ( i < f ) {
+      val ch = if ( str.charAt(i) == str.charAt(i+1) ) '1' else '0'
+      buf.append(ch)
+      i = i + 2
+    }
+    buf.toString()
+  }
+
+  def part2_p(): Unit = { //2 thread version, for fun
+    var checkSum = getStr(length2)
+    while ( checkSum.length % 2 == 0 ) {
+      val size = checkSum.length/2
+      checkSum = if ( size % 2 > 0 ) calculateSum(0, checkSum.length, checkSum) else {
+        List(0,size).map( i => Future{ calculateSum(i,i+size, checkSum)}).map( f=> {
+          Await.ready(f, Duration.Inf)
+          f.value.get.get
+        }).foldLeft("")(_ + _)
+      }
+    }
+    println(checkSum)
+  }
+
   def main(args: Array[String]): Unit = {
     part1()
     part2()
+    part2_p()
   }
 
 }
